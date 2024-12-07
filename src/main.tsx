16e3f6edb313712fae6a899a21a54b71a26b7ee9
ankushKun <ankush4singh@gmail.com>
2024-12-07 19:50:32 +0530

    deploy hoga bhai

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Index from './app'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./auth";
+import Scene from "./scene";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { baseSepolia } from "viem/chains";
+// Simply place this at the top of your application's entry point to have the components working out of the box.
+import '@coinbase/onchainkit/styles.css';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Index />
+    <OnchainKitProvider apiKey="1mgDEk4iV5TOEEPwvTEB96K8P07GYCBa" chain={baseSepolia}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Scene />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </OnchainKitProvider>
   </StrictMode>,
 )
